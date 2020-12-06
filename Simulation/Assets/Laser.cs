5131c1153751f6845833aad8485ba7bd46140e83
martinasamoule <61949296+martinasamoule@users.noreply.github.com>
2020-12-07 01:13:51 +0200

    Add files via upload

---
+++ Simulation/Assets/Laser.cs
@@ -0,0 +1,52 @@
+﻿
+
+public class Laser 
+{
+    public Color laserColor = new Color (0 , 1 , 0 , 0.5f );
+    public float distanceLaser = 50 ;
+    public float finalLength = 0.02f ;
+    public float initialLength = 0.02f ;
+
+    private Vector3 positionLaser ;
+    private LinerRemderer linerRemderer ;
+    private float distance = 0 ;
+    
+
+     void Start ()
+  {
+      distance = distanceLaser ;
+      positionLaser = new Vector3 (0 , 0 , finalLength) ;
+      linerRemderer = gameObject.AddComponent<LinerRemderer>();
+      linerRemderer.material = new Material (Shader.Find("particles/Additive")) ;
+      linerRemderer.startColor = laserColor ;
+      linerRemderer.endColor = laserColor ;
+      linerRemderer.startWidth = initialLength ;
+      linerRemderer.endWidth = finalLength ;
+      linerRemderer.positionCount = 2 ;
+  }
+
+  void Update ()
+  {
+     Vector3 finalPoint = transform.position + transform.forward * distanceLaser ;
+     RaycastHit collisionPoint ;
+     if (Physics.Raycast(transform.position , transform.forward , out collisionPoint , distanceLaser))
+     {
+         linerRemderer.SetPostion(0 , transform.position) ;
+         linerRemderer.SetPostion(1 , collisionPoint.point) ;
+         distance = collisionPoint.distance ;
+     }
+     else
+     {
+         linerRemderer.SetPostion(0 , transform.position) ;
+         linerRemderer.SetPostion(1 , finalPoint) ; 
+         distance = distanceLaser ;
+     }
+  }
+ 
+ 
+ public void getDistance ()
+      {
+          return distance ;
+      }
+
+}
---
+++ Simulation/Assets/Lasers.cs
@@ -0,0 +1,52 @@
+﻿
+public class Lasers 
+{
+  public Color colorBeam = new Color (0 , 255 , 0 , 0.5f );
+  public int distanceLaser = 20 ;
+  public static int lasers = 8 ;
+  public static int view = 120 ;
+  public float height = 0 ;
+
+  private int count ;
+  private GameObject [] laserObjects ;
+
+
+  void Start ()
+  {
+      count = view / (lasers - 1);
+      laserObjects = new GameObject [lasers] ;
+      for(int i = 0 ; i < lasers ; i ++ )
+      {
+          float currentDegree = count * i - view / 2 ;
+          GameObject obj = new GameObject () ;
+          Laser laser = obj.AddComponent<Laser>() ;
+          laser.finalLength = 0.02f ;
+          laser.laserColor = colorBeam ;
+          laser.distanceLaser = distanceLaser ;
+          laser.transform.position = new Vector3 (transform.position.x , transform.position.y+height , transform.position.z) ;
+          laserObjects[i] = obj;
+          laserObjects[i].transform.Rotate(new Vector3(0 , currentDegree , 0));
+		  obj.transform.SetParent(transform); //alstr dh mkn4 mktob flvideo bs ktbeno mn awl video
+      }
+  }
+
+  public float[] getDistance()
+{	float[] lasers = new float[laserObjects.Length];
+	for (int i = 0 ; i < laserObjects.Length ; i++)
+	{
+		lasers[i] = laserObjects[i].GetComponent<Laser>().getDistance();
+	}
+	return lasers;
+}
+
+  void Update ()
+  {
+      foreach(GameObject obj in laserObjects)
+      {
+          obj.transform.position = new Vector3 (transform.position.x , transform.position.y+height , transform.position.z) ; 
+      }
+
+  }
+
+
+}
